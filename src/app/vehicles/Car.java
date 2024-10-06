e9595c5698f1c56ee2ce7cd72de88dea0b43f37a
Jordan <A00326246@student.tus.ie>
2024-10-06 13:53:27 +0100

    Added class for car and vehicle with inheritance

---
+++ src/app/vehicles/Car.java
@@ -0,0 +1,9 @@
+package app.vehicles;
+
+public class Car extends Vehicle {
+
+    public Car(String make, String model, int year) {
+        super(make, model, year);
+    }
+
+}
---
+++ src/app/vehicles/Vehicle.java
@@ -0,0 +1,29 @@
+package app.vehicles;
+
+public class Vehicle {
+    private String make;
+    private String model;
+    private int year;
+
+    public Vehicle(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void displayInfo() {
+        System.out.println("Vehicle Info: " + year + " " + make + " " + model);
+    }
+}
