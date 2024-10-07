a0ca7427e0bb9e0e12e92a046ef59dcc6ee27469
Jordan <A00326246@student.tus.ie>
2024-10-07 13:52:24 +0100

    Implemented motorbike class that also inherits from vehicle similarly to car

---
+++ src/app/vehicles/Motorbike.java
@@ -0,0 +1,27 @@
+package app.vehicles;
+import app.services.ServiceRecord;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Motorbike extends Vehicle {
+    private List<ServiceRecord> serviceHistory;
+
+    public Motorbike(String make, String model, int year) {
+        super(make, model, year);
+        this.serviceHistory = new ArrayList<>();
+    }
+
+    public void addService(ServiceRecord service) {
+        serviceHistory.add(service);
+    }
+
+    public List<ServiceRecord> getServiceHistory() {
+        return new ArrayList<>(serviceHistory);
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Service History: " + (serviceHistory.isEmpty() ? "No services yet." : serviceHistory.size() + " services"));
+    }
+}
