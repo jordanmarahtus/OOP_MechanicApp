745667d2290e83d8c079afeaa89997af38903dbd
Jordan <A00326246@student.tus.ie>
2024-10-14 18:52:09 +0100

    Added functionality to add car through management system cmd line

--- src/MechanicManagementSystem.java
+++ src/MechanicManagementSystem.java
@@ -1,11 +1,14 @@
 
 import app.mechanic.Mechanic;
+import app.vehicles.Car;
 
 import java.util.Scanner;
 public class MechanicManagementSystem {
     private static Scanner scanner = new Scanner(System.in);
     private boolean running = true;
     private int mechanicId = 0;
+    private int carId = 0;
+
     public void start() {
         while (running) {
             printMenu();
@@ -15,6 +18,9 @@ public class MechanicManagementSystem {
                     addMechanic();
                     break;
                 case 2:
+                    addCar();
+                    break;
+                case 3:
                     running = false;
                     System.out.println("Exiting system...");
                     break;
@@ -28,16 +34,33 @@ public class MechanicManagementSystem {
         String name = scanner.nextLine();
         System.out.print("Enter job: ");
         String job = scanner.nextLine();
+
         Mechanic mechanic = new Mechanic(name, mechanicId, job);
         System.out.println("Mechanic added");
         mechanic.displayInfo();
         mechanicId++;
     }
 
+    private void addCar() {
+
+        System.out.print("Enter year: ");
+        int year = scanner.nextInt();
+        System.out.print("Enter make: ");
+        String make = scanner.nextLine();
+        System.out.print("Enter model: ");
+        String model = scanner.nextLine();
+
+        Car car = new Car(make, model, year);
+        System.out.println("Car added" + make + " " + model);
+        carId++;
+
+    }
+
     private void printMenu() {
         System.out.println("Mechanic Management System");
         System.out.println("1. Add Mechanic");
-        System.out.println("2. Exit");
+        System.out.println("2. Add Car");
+        System.out.println("3. Exit");
         System.out.print("Enter your choice: ");
     }
 }
